1bd717fcf40155d8777daa6015e67550139cd225
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-04-05 05:20:12 +0300

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,20 +13,30 @@ def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
-    if exists(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        put(archive_path, '/tmp')
+
+        archive_filename = os.path.basename(archive_path)
+        archive_name = os.path.splitext(archive_filename)[0]
+
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive_filename, archive_name))
+        run('rm /tmp/{}'.format(archive_filename))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(archive_name, archive_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(archive_name))
+
+        print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
+        print(e)
         return False

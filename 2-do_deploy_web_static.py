a0a583b410985fb0102d7e8fe4a19de85cc527f3
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-04-05 05:26:45 +0300

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,26 +17,31 @@ def do_deploy(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp')
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
 
-        archive_filename = os.path.basename(archive_path)
-        archive_name = os.path.splitext(archive_filename)[0]
+        # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
+        filename = os.path.basename(archive_path)
+        folder_name = '/data/web_static/releases/' + filename.split('.')[0]
+        run('mkdir -p {}'.format(folder_name))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
 
-        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_filename, archive_name))
-        run('rm /tmp/{}'.format(archive_filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(archive_name, archive_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_name))
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(filename))
+
+        # Move the contents of the extracted folder to the parent directory
+        run('mv {}/web_static/* {}'.format(folder_name, folder_name))
+
+        # Delete the now-empty folder
+        run('rm -rf {}/web_static'.format(folder_name))
+
+        # Delete the symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(archive_name))
+
+        # Create a new symbolic link to the new version
+        run('ln -s {} /data/web_static/current'.format(folder_name))
 
         print("New version deployed!")
         return True
-
-    except Exception as e:
-        print(e)
+    except:
         return False

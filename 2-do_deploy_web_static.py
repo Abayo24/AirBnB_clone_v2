5e227ec2ba7caa35f22b01f71fd3060e12a5d38f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-05-16 00:45:55 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script that distributes an archive to your web servers \
+        using the function do_deploy.
 """
 from datetime import datetime
 from fabric.api import *
